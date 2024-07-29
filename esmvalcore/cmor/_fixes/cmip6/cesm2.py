269d490e3d5da88c55058889b78d06b91a4e3410
Britta Grusdt <bgrusdt@gmail.com>
2024-07-29 13:40:46 +0200

    added fix for metadata region vs. basin

--- esmvalcore/cmor/_fixes/cmip6/cesm2.py
+++ esmvalcore/cmor/_fixes/cmip6/cesm2.py
@@ -1,5 +1,6 @@
 """Fixes for CESM2 model."""
 from shutil import copyfile
+import iris
 
 import numpy as np
 from netCDF4 import Dataset
@@ -13,6 +14,53 @@ from ..shared import (
     add_scalar_typesea_coord,
     fix_ocean_depth_coord,
 )
+
+class Msftmz(Fix):
+    """Fixes for discrete DimCoord, code taken from: https://github.com/ESMValGroup/ESMValCore/compare/main...dhohn:fix_datasets_amoc?expand=1#:~:text=class%20Msftmz(,super().fix_metadata(cubes)"""
+
+    @staticmethod
+    def transform_region_coord(coord: iris.coords.DimCoord) -> iris.coords.AuxCoord:
+        """transform a DimCoord with indexes as points to AuxCoord with names as points
+        Parameters
+        ----------
+        coord: iris.coords.DimCoord
+        Returns
+        -------
+        iris.coords.AuxCoord
+        
+        """
+        # parses string like: 'atlantic_arctic_ocean=0, indian_pacific_ocean=1, global_ocean=2'
+        lookup = { int(p[1]):p[0] for p in map(lambda x: x.split("="), coord.attributes['requested'].split(",")) }
+        new_points = ['']*len(lookup)
+        for old_label in coord.points:
+            new_points[old_label] = lookup[old_label]
+        new_coord = iris.coords.AuxCoord(new_points, 
+                                         standard_name="region", 
+                                         var_name="basin", 
+                                         long_name="ocean basin",
+                                         units="no unit")
+        return new_coord
+
+    def fix_metadata(self, cubes):
+        """Fix for region DimCoord with indexes as points
+        to AuxCoord with names as points
+        Parameters
+        ----------
+        cubes: iris CubeList
+            List of cubes to fix
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        cube = self.get_cube_from_list(cubes)
+        coord = cube.coord("region")
+        new_coord = self.transform_region_coord(coord=coord)
+        dims = cube.coord_dims("region")
+        for cube in cubes:
+            cube.remove_coord("region")
+            cube.add_aux_coord(new_coord, dims)
+        return cubes
+        # return super().fix_metadata(cubes)
 
 
 class Cl(Fix):
@@ -94,6 +142,7 @@ class Cl(Fix):
         lev_coord.points = a_coord.core_points() + b_coord.core_points()
         lev_coord.bounds = a_coord.core_bounds() + b_coord.core_bounds()
         lev_coord.units = '1'
+
         return cubes
 
 
