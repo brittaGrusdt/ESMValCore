223531f34cbd8d7b66dcc9b424d5607318cdb0c6
brgrus001 <bgrusdt@gmail.com>
2024-08-09 14:36:33 +0200

    added amoc fixes for CESM2_FV2, CESM2_WACCM, CESM2_WACCM_FV2, E3SM_1_0

--- esmvalcore/cmor/_fixes/cmip6/cesm2_fv2.py
+++ esmvalcore/cmor/_fixes/cmip6/cesm2_fv2.py
@@ -4,6 +4,9 @@ from .cesm2 import Fgco2 as BaseFgco2
 from .cesm2 import Omon as BaseOmon
 from .cesm2 import Tas as BaseTas
 from ..common import SiconcFixScalarCoord
+from ..fix import Fix
+
+import iris
 
 
 Cl = BaseCl
@@ -25,3 +28,52 @@ Siconc = SiconcFixScalarCoord
 
 
 Tas = BaseTas
+
+
+class Msftmz(Fix):
+    """Fixes for discrete DimCoord, code taken from: https://github.com/ESMValGroup/ESMValCore/compare/main...dhohn:fix_datasets_amoc?expand=1#:~:text=class%20Msftmz(,super().fix_metadata(cubes)"""
+
+    @staticmethod
+    def transform_region_coord(coord: iris.coords.DimCoord) -> iris.coords.AuxCoord:
+        """transform a DimCoord with indexes as points to AuxCoord with names as points
+        Parameters
+        ----------
+        coord: iris.coords.DimCoord
+        Returns
+        -------
+        iris.coords.AuxCoord
+        
+        """
+        # parses string like: 'atlantic_arctic_ocean=0, indian_pacific_ocean=1, global_ocean=2'
+        lookup = { int(p[1]):p[0] for p in map(lambda x: x.split("="), coord.attributes['requested'].split(",")) }
+        new_points = ['']*len(lookup)
+        for old_label in coord.points:
+            new_points[old_label] = lookup[old_label]
+        new_coord = iris.coords.AuxCoord(new_points, 
+                                         standard_name="region", 
+                                         var_name="basin", 
+                                         long_name="ocean basin",
+                                         units="no unit")
+        return new_coord
+
+    def fix_metadata(self, cubes):
+        """Fix for region DimCoord with indexes as points
+        to AuxCoord with names as points
+        Parameters
+        ----------
+        cubes: iris CubeList
+            List of cubes to fix
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        cube = self.get_cube_from_list(cubes)
+        coord = cube.coord("region")
+        new_coord = self.transform_region_coord(coord=coord)
+        dims = cube.coord_dims("region")
+        for cube in cubes:
+            cube.remove_coord("region")
+            cube.add_aux_coord(new_coord, dims)
+        return cubes
+        # return super().fix_metadata(cubes)
+
