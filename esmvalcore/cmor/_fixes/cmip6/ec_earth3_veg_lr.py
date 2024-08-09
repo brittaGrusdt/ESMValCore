6b2d40ce4aa202d51583f18456469cd7ad321dc6
Britta Grusdt <bgrusdt@gmail.com>
2024-08-09 13:52:07 +0200

    added fix, since for  1934, dimension names were different

--- esmvalcore/cmor/_fixes/cmip6/ec_earth3_veg_lr.py
+++ esmvalcore/cmor/_fixes/cmip6/ec_earth3_veg_lr.py
@@ -1,5 +1,20 @@
 """Fixes for EC-Earth3-Veg-LR model."""
-from ..common import OceanFixGrid
 
+from ..common import OceanFixGrid
+from ..fix import Fix
 
 Siconc = OceanFixGrid
+
+
+class Tos(Fix):
+    def fix_metadata(self, cubes):
+        """Fix metadata."""
+
+        dim1 = "cell index along first dimension"
+        dim2 = "cell index along second dimension"
+        for cube in cubes:
+            if cube.attributes.get("variant_label", "") == "r2i1p1f1":
+                cube.dim_coords[1].rename(dim2)
+                cube.dim_coords[2].rename(dim1)
+
+        return cubes
